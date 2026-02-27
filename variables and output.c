99741e7af442ab3f741ff943a6e839526390f3de
tuanapi <tuanapi@proton.me>
2026-02-27 23:10:38 +0300

    Add files via upload
    The first C program I wrote

---
+++ variables and output.c
@@ -0,0 +1,9 @@
+// My first program in C!
+#include <stdio.h>
+
+int main(){
+    char* /* Where da string D:? */ msg = "I have";
+    int appleCount = 5;
+    printf("%s %d apples!\n", msg, appleCount);
+    return 0;
+}
